5b396179ba6f5150e00fe0511815265fba8b732c
Taline Araujo <taline.araujo@hotmail.com>
2024-08-24 01:20:06 -0300

    configurações do RBAC

--- src/routes/rbac.route.js
+++ src/routes/rbac.route.js
@@ -7,5 +7,8 @@ RbacRouter.get("/listPermissions", RbacController.listPermissions)
 RbacRouter.get("/listRoles", RbacController.listRoles)
 RbacRouter.post("/createOnePermission", RbacController.createOnePermission)
 RbacRouter.post("/createOneRole", RbacController.createOneRole)
+RbacRouter.post("/addPermissionToRole", RbacController.addPermissionToRole)
+RbacRouter.post("/addRoleToUser", RbacController.addRoleToUser)
+RbacRouter.get("/listPermissionsByRole/:id", RbacController.listPermissionsByRole)
 
 module.exports = RbacRouter
